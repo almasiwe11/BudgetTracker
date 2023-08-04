072ef953534f4ad6ae61a40a750bb35ce9229b70
Almas Akhmetov <awakenedduck@gmail.com>
2023-08-04 17:07:19 +0600

    add today red visua

--- src/DayCell.jsx
+++ src/DayCell.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { sub, format, isSameMonth } from "date-fns";
+import { sub, format, isSameMonth, isSameDay } from "date-fns";
 const DayCell = ({ day, today }) => {
   const formattedDay = format(day, "d");
   const wrongMonth = !isSameMonth(today, day);
+  let markToday;
+  if (isSameDay(new Date(), day)) markToday = true;
 
   return (
-    <div className={`dayCell ${wrongMonth && "wrongMonth"}`}>
-      {formattedDay}
+    <div className={`dayCell ${wrongMonth && "wrongMonth"} `}>
+      <div className={`${markToday && "today"} dayNumber`}> {formattedDay}</div>
     </div>
   );
 };
