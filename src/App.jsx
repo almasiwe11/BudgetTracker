39647b93681345ce06cf8281fc0295971d875c7b
Almas Akhmetov <awakenedduck@gmail.com>
2023-08-12 14:57:47 +0600

    will be making responsive

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
           selectedDay={selectedDay}
         />
       </>
-      <AllApps />
+      {/* <AllApps /> */}
       <CalendarGrid
         today={today}
         setToday={setToday}
